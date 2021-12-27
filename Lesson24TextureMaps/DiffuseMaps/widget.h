e097e6ed9ecb2d7986aa5bae8cd0802f5b3a4105
Aleksei Pripolzin <apripolzin@gmail.com>
2021-12-27 17:05:28 +0100

    Lesson 2.4 TextureMaps

---
+++ Lesson24TextureMaps/DiffuseMaps/widget.h
@@ -0,0 +1,55 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
+#include <QOpenGLBuffer>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLVertexArrayObject>
+#include <QScopedPointer>
+
+#include "camera.h"
+
+class QOpenGLTexture;
+
+class Widget : public QOpenGLWidget, protected QOpenGLFunctions
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+
+    void keyPressEvent(QKeyEvent *event) override;
+    void keyReleaseEvent(QKeyEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void wheelEvent(QWheelEvent *event) override;
+
+private:
+    void initializeCubeGeometry();
+    void initializeLampGeometry();
+    void initializeShaders();
+    QOpenGLTexture *initializeTexture(const QString &path);
+
+
+    void drawCube();
+    void drawLamp();
+
+    QOpenGLBuffer cubeBuffer;
+    QOpenGLBuffer lampBuffer;
+    QOpenGLVertexArrayObject cubeVao;
+    QOpenGLVertexArrayObject lampVao;
+    QOpenGLShaderProgram cubeProgram;
+    QOpenGLShaderProgram lampProgram;
+    QOpenGLTexture *container = nullptr;
+
+    const QVector3D lightPos = {1.2f, 1.0f, 2.0f};
+
+    Camera camera;
+};
+#endif // WIDGET_H
---
+++ Lesson24TextureMaps/Ex2InvertColors/fcube.fsh
@@ -0,0 +1,48 @@
+#version 330 core
+out vec4 color;
+
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TexCoords;
+
+uniform vec3 viewPos;
+
+struct Material {
+    vec3      ambient;
+    sampler2D diffuse;
+    sampler2D specular;
+    float     shininess;
+};
+
+uniform Material material;
+
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Light light;
+
+void main()
+{
+    // Ambient
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+
+    // Diffuse
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+
+    // Specular
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * spec * (vec3(1.0f) - vec3(texture(material.specular, TexCoords)));
+
+    vec3 result = (ambient + diffuse + specular);
+    color = vec4(result, 1.0f);
+}
